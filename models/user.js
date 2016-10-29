341eb789b74ce01c42dd9d91965769bc694b6d5a
Matt Leach <email@mattleach.info>
2016-10-30 00:23:57 +0100

    User registration

--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ var mongooseUniqueValidator = require('mongoose-unique-validator')
 
 var schema = new Schema({
 	firstName: {type: String, required: true},
-	lastName {type: String, required: true},
-	password {type: String, required: true},
-	email {type: String, required: true, unique: true},
-	messages [{type: Schema.Types.ObjectId, ref: 'Message'}]
+	lastName: {type: String, required: true},
+	password: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
 });
 
 schema.plugin(mongooseUniqueValidator);
